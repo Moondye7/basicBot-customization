69be7347768078a235aaad0e85d554813a933c96
Moondye7 <karottenkopf@arcor.de>
2015-07-14 22:28:00 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -81,7 +81,7 @@
         historySkip: true,
         timeGuard: true,
         maximumSongLength: 10,
-        autodisable: true,
+        autodisable: false,
         commandCooldown: 30,
         usercommandsEnabled: true,
         skipPosition: 3,
