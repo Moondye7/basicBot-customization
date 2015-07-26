750f3dd1fc6ff200cf5e9833d28b39bcf8bfada1
Daniel <karottenkopf@arcor.de>
2015-07-26 05:20:36 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -162,7 +162,7 @@
         motd: "( ͡° ͜◯ ͡°) ＣＬＯＷＮ ＦＩＥＳＴＡ ( ͡° ͜◯ ͡°)",
         socialEnabled: true,
         socialInterval: 15,
-        social: "Hier findest du alle Moondye7 Social Links: Twitch http://www.twitch.tv/moondye7 <br> Twitter https://twitter.com/Moondye7 Twitter <br> Youtube https://www.youtube.com/user/Moondye7 Youtube <br> Facebook https://www.facebook.com/Moondye7 Facebook",
+        social: "Hier findest du alle Moondye7 Social Links: Twitch http://www.twitch.tv/moondye7 Twitter https://twitter.com/Moondye7 Twitter Youtube https://www.youtube.com/user/Moondye7  Facebook https://www.facebook.com/Moondye7",
         filterChat: false,
         etaRestriction: false,
         welcome: false,
